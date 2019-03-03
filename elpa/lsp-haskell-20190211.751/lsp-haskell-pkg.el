a1cdb49d9e7ed157620c86167f285d35bcaa24e5
Karl Saehun Chung <nycom13@gmail.com>
2019-03-03 23:00:05 +0900

    save

---
+++ elpa/lsp-haskell-20190211.751/lsp-haskell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-haskell" "20190211.751" "Haskell support for lsp-mode" '((lsp-mode "3.0") (haskell-mode "1.0")) :commit "df7ac24332917bcd8463988767d82f17da46b77c" :keywords '("haskell") :url "https://github.com/emacs-lsp/lsp-haskell")
